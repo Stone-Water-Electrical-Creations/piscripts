fb5ad304b0117f009c1528d9f6634189540e7e64
Russ Baughcum <rbaughcum@gmail.com>
2019-11-09 21:26:56 -0500

    Finally got directory creation for new files

--- moveTVFilesToDirs.sh
+++ moveTVFilesToDirs.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+if [ "$2" = "debug" ] ; then
+    set -xv
+fi
 usage() {
 cat <<EOM
 Usage:
@@ -12,8 +15,23 @@ cd $sourceDir
 touch rm.tmp 
 find .  -maxdepth 1 -type f | egrep -v '.fuse|rm.tmp' | while read filename
 do 
-    dir=`echo $filename|awk -F\- '{print $1}'|sed -e 's/ The //g' -e 's/ /\\ /g'`
+set -xv
+    dir=`echo $filename|awk -F\- '{print $1}'`
+    dir=`echo $dir |sed -e 's/ The//g' | sed -e 's/ /\\ /g' | sed -e 's/.\///g' |sed -e 's/ ^//g'`
+    ls "$dir" &>/dev/null
+    if [ $? = 2 ] ; then
+        echo "$dir"
+        echo mkdir "$dir">> rm.tmp
+    fi
     echo mv `echo $filename|sed -e 's/ /\\\ /g'` `echo $dir|sed -e 's/ /\\\ /g'`>> rm.tmp 
+    set +xv
 done 
-bash rm.tmp 
-rm rm.tmp
+if [ "$2" = "debug" ] ; then
+   more rm.tmp
+   rm rm.tmp
+   set +xv
+   exit 0
+else
+    bash rm.tmp 
+    rm rm.tmp
+fi
